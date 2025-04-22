de94360b6eace14e16882b7df3aa8bb3a384b16b
leopantsulaia <leopantsulaia@gmail.com>
2025-04-23 00:46:09 +0400

    finally deployed some things are just ironical but it's a plain text so...

--- src/components/Body/StylesFunction.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function StylesFunction() {
-  return (
-    <style id="rs-plugin-settings-inline-css" type="text/css">
-      {`#rs-demo-id { }`}
-    </style>
-  );
-}
